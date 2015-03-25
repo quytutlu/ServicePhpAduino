a754359d81169a22d4d4283c9618a5f750686f05
quytutlu <quytutlu@gmail.com>
2015-03-25 23:58:07 +0700

    update BLL
    vì xóa nhầm

---
+++ BLL.php
@@ -0,0 +1,106 @@
+<?php
+	include_once "DAL.php";
+	include_once "Object/TrangThai.php";
+	require 'PHPMailer/PHPMailerAutoload.php';
+	class XuLyNghiepVu
+	{
+		private $ThaoTacCSDL;
+		public function XuLyNghiepVu()
+		{
+			$this->ThaoTacCSDL=new TruyXuatDuLieu();
+		}
+		public function BatThietBi($TenThietBi)
+		{
+			return $this->ThaoTacCSDL->BatThietBi($TenThietBi);
+		}
+		public function TatThietBi($TenThietBi)
+		{
+			return $this->ThaoTacCSDL->TatThietBi($TenThietBi);
+		}
+		public function RaNgoai()
+		{
+			return $this->ThaoTacCSDL->RaNgoai();
+		}
+		public function LayTrangThai($cmd)
+		{
+			$kq=$this->ThaoTacCSDL->LayTrangThai($cmd);
+			$ListTrangThai;
+			while($row=mysql_fetch_array($kq))
+			{
+				$temp=new TrangThaiThietBi();
+				$temp->TenThietBi=$row[1];
+				$temp->TrangThai=$row[3].$row[2];
+				$ListTrangThai[]=$temp;
+			}
+			return $ListTrangThai;
+		}
+		private function LayEmail()
+		{
+			$kq=$this->ThaoTacCSDL->LayEmail();
+			return mysql_fetch_array($kq)[0];
+		}
+		public function GuiEmail($NhietDo)
+		{
+			echo "gui mail";
+			$email=$this->LayEmail();
+			$mail = new PHPMailer();
+			// Thiết lập SMTP
+			$mail->IsSMTP();                // thiết lập 1 kết nối đến SMTP
+			$mail->IsHTML(true);
+			//$mail->SMTPDebug  = 4;        // in ra màn hình thông tin debug
+			$mail->SMTPAuth = true;        	// sử dụng tài khoản email để gửi
+			$mail->SMTPSecure = "ssl";      // sử dụng kết nối tls
+			$mail->Host = "ssl://smtp.gmail.com";
+			$mail->Port = 465;
+			$mail->Encoding = '7bit';       // SMS uses 7-bit encoding
+
+			$mail->Username   = "nhathongminharduino@gmail.com"; // Tài khoản email gửi
+			$mail->Password   = "654321a@"; // mật khẩu
+			$mail->FromName="Arduino";
+
+			// thiết lập 1 email
+			$mail->CharSet="utf-8";
+			$mail->Subject = "Cảnh báo từ ngôi nhà thông minh";     // Chủ đề email
+			$mail->Body = "<h1>-Cảnh báo nhiệt độ trong nhà hiện tại là ".$NhietDo."</h1>\r\n<h1>-Nhiệt độ này có thể nhà bạn đang có sự cố hãy kiểm tra</h1>";        // nội dung email
+			 
+			// email nhận
+			$mail->AddAddress( $email );
+			$mail->send();      // Gửi mail!
+		}
+		public function UpdateNhietDo($NhietDo)
+		{
+			if($NhietDo>40)
+			{
+				$kq=$this->ThaoTacCSDL->KiemTraCoNguoi();
+				$row=mysql_fetch_array($kq);
+				if($row[1]==0)
+				{
+					$this->GuiEmail($NhietDo);
+				}
+				else
+				{
+					//echo "bao dong chay";
+				}
+			}
+			if($NhietDo>=35&&$NhietDo<=40)
+			{
+				$kq=$this->ThaoTacCSDL->KiemTraCoNguoi();
+				$row=mysql_fetch_array($kq);
+				if($row[1]==1)
+				{
+					$this->BatThietBi("quatdien1");
+				}
+			}
+			return $this->ThaoTacCSDL->UpdateNhietDo($NhietDo);
+		}
+		public function UpdateStt($MaStt)
+		{
+			return $this->ThaoTacCSDL->UpdateStt($MaStt);
+		} 
+		public function DangNhap($TenDangNhap,$MatKhau)
+		{
+			$kq=$this->ThaoTacCSDL->DangNhap($TenDangNhap,$MatKhau);
+			return mysql_num_rows($kq)==1;
+		}
+	}
+?>
